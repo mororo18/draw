aa70be5792f919bf5177543ec3bc9504e5acda6d
Victor <victormororo@gmail.com>
2024-05-22 00:17:02 -0300

    arquivos iniciais

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod draw;
+
+// https://handmade.network/forums/articles/t/2834-tutorial_a_tour_through_xlib_and_related_technologies
+// events -> https://www.oreilly.com/library/view/xlib-reference-manual/9780937175262/13_appendix-e.html
+// xlib c header -> https://codebrowser.dev/gtk/include/X11/X.h.html
+fn main() {
+    let mut win =  draw::Window::new(800, 600);
+    win.run();
+}

149d6be61d85a6fffef613390377318de7750dcd
Victor <victormororo@gmail.com>
2024-05-23 00:01:18 -0300

    window module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 mod draw;
+mod window;
 
-// shared memory iamge -> https://www.x.org/releases/X11R7.7/doc/xextproto/shm.html
-// https://handmade.network/forums/articles/t/2834-tutorial_a_tour_through_xlib_and_related_technologies
-// events -> https://www.oreilly.com/library/view/xlib-reference-manual/9780937175262/13_appendix-e.html
-// xlib c header -> https://codebrowser.dev/gtk/include/X11/X.h.html
-// demowindow example -> https://docs.rs/x11/latest/src/input/input.rs.html#121
-// input example -> https://who-t.blogspot.com/2009/05/xi2-recipes-part-1.html
+use window::{
+    Window,
+    Event,
+};
+
 fn main() {
-    let mut win =  draw::Window::new(800, 600);
-    win.run();
+    let mut win =  Window::new(800, 600);
+
+    let mut window_open = true;
+
+    while window_open {
+        let events: Vec<Event> = win.handle();
+
+        for e in events.iter() {
+            if *e == Event::CloseWindow {
+                window_open = false;
+            }
+        }
+
+    }
 }

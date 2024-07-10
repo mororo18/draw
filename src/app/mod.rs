c39840f4438a3486035a3f92f23003350356c3ca
Victor <victormororo@gmail.com>
2024-07-10 13:58:21 -0300

    Application struct

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,1 +1,99 @@
 pub mod window;
+
+use crate::renderer::scene::Scene;
+
+use window::{
+    Window,
+    Event,
+    Key,
+};
+
+
+pub
+struct Application {
+    scene: Scene,
+    win: Window,
+}
+
+impl Application {
+    pub
+    fn new () -> Self {
+        let width = 800;
+        let height = 600;
+
+        Self {
+            scene:  Scene::new(width, height),
+            win:    Window::new(width, height),
+        }
+    }
+
+    pub
+    fn run (&mut self) {
+
+        let frame_rate: f32 = 60.0;
+        let dt_ms = 1000.0 / frame_rate;
+
+        let mut now = std::time::Instant::now();
+
+        let mut window_open = true;
+
+        while window_open {
+            let events: Vec<Event> = self.win.handle();
+
+            for e in events.iter() {
+
+                match e {
+                    Event::CloseWindow => {
+                        window_open = false;
+                    },
+
+                    Event::KeyPress(key) => {
+                        print!("KeyPress ");
+                        match key {
+                            Key::UpArrow => {
+                                println!("Up");
+                                self.scene.camera_up();
+                            },
+                            Key::DownArrow => {
+                                println!("Down");
+                                self.scene.camera_down();
+                            },
+                            Key::LeftArrow => {
+                                println!("Left");
+                                self.scene.camera_left();
+                            },
+                            Key::RightArrow => {
+                                println!("Right");
+                                self.scene.camera_right();
+                            },
+
+                            _ => {println!("Unknow")},
+                        };
+                    },
+
+                    Event::KeyRelease(_key) => {
+                    },
+
+                    _ => {},
+                };
+
+            }
+
+
+
+            let elapsed = now.elapsed().as_millis() as f32;
+            if elapsed > dt_ms {
+                now = std::time::Instant::now();
+                println!("FPS {}", 1000.0 / elapsed);
+                //scene.camera_right();
+
+                self.scene.render();
+                //let render_elapsed = now.elapsed().as_millis() as f32;
+                //println!("Rendering percentage {}%", render_elapsed * 100.0 / dt_ms);
+
+                let frame_slice = self.scene.frame_as_bytes_slice();
+                self.win.write_frame_from_slice(frame_slice);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,9 @@
-use draw::app::window::{
-    Window,
-    Event,
-    Key,
-};
-
-use draw::renderer::scene::{
-    Scene,
-};
-
-use std::time::Instant;
+use draw;
 
 fn main() {
 
-    let width = 800;
-    let height = 600;
-    let mut win =  Window::new(width, height);
+    let mut app = draw::app::Application::new();
 
-    let mut window_open = true;
-
-    //let mut canva = Canva::new(width, height);
-    let mut scene = Scene::new(width, height);
-
-
-    let mut now = Instant::now();
-    //let frame_rate: f32 = 1.0;
-    let frame_rate: f32 = 60.0;
-    let dt_ms = 1000.0 / frame_rate;
-
-    // CREATE SCENE = OBJECTS + CAMERA
-
-    while window_open {
-        let events: Vec<Event> = win.handle();
-
-        for e in events.iter() {
-
-            match e {
-                Event::CloseWindow => {
-                    window_open = false;
-                },
-
-                Event::KeyPress(key) => {
-                    print!("KeyPress ");
-                    match key {
-                        Key::UpArrow => {
-                            println!("Up");
-                            scene.camera_up();
-                        },
-                        Key::DownArrow => {
-                            println!("Down");
-                            scene.camera_down();
-                        },
-                        Key::LeftArrow => {
-                            println!("Left");
-                            scene.camera_left();
-                        },
-                        Key::RightArrow => {
-                            println!("Right");
-                            scene.camera_right();
-                        },
-
-                        _ => {println!("Unknow")},
-                    };
-                },
-
-                Event::KeyRelease(_key) => {
-                },
-
-                _ => {},
-            };
-
-        }
-
-
-
-        let elapsed = now.elapsed().as_millis() as f32;
-        if elapsed > dt_ms {
-            now = Instant::now();
-            println!("FPS {}", 1000.0 / elapsed);
-            //scene.camera_right();
-
-            scene.render();
-            //let render_elapsed = now.elapsed().as_millis() as f32;
-            //println!("Rendering percentage {}%", render_elapsed * 100.0 / dt_ms);
-
-            let frame_slice = scene.frame_as_bytes_slice();
-            win.write_frame_from_slice(frame_slice);
-        }
-    }
+    app.run();
 
 }
